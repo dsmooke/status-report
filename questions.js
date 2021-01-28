29cc27c0a8a26d0f88ecf1a364657e8f72f55fcd
Danny Smooke <dana.smooke@gmail.com>
2021-01-28 17:38:18 -0500

    created questions.js file with questions array, added first prompt: what would you like to do?

--- questions.js
+++ questions.js
@@ -4,17 +4,21 @@ const util = require("util");
 
 const questions = [
     {
-        type:
-            name:
-        message: "What would you like to do?"
-        choices: 
-    
-    },
+        name: "doWhat",
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+            "View All Employees",
+            "View All Employees By Department",
+            "View All Employees By Manager",
+            "Add Employee",
+            "Remove Employee",
+            "View All Roles",
+            "Add Role",
+            "Remove Role",
+            "Update Employee Role",
+            "Update Employee Manager"
+        ]
 
-    {
-        type:
-            name:
-        message:
-    
-    },
-]
+    }
+];
