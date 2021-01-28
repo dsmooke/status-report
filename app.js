326dc8acf7fb80fad3196f7935726659f23defd3
Danny Smooke <dana.smooke@gmail.com>
2021-01-28 17:17:22 -0500

    installed console.table package, const cTable require

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("util");
 
+const cTable = require("console.table");
+
 const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -16,3 +18,5 @@ connection.connect((error) => {
     console.log("Connected to Database");
     init();
 });
+
+
