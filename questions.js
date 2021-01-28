b050951f39dca8476e5b943944a6aae9edf0a2ef
Danny Smooke <dana.smooke@gmail.com>
2021-01-28 13:33:17 -0500

    created questions.js file to hold inquirer questions array of objects

---
+++ questions.js
@@ -0,0 +1,20 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+
+const questions = [
+    {
+        type:
+            name:
+        message: "What would you like to do?"
+        choices: 
+    
+    },
+
+    {
+        type:
+            name:
+        message:
+    
+    },
+]
