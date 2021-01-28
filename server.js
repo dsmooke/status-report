29eb927ad98651e46bd5964bfb7dfb3840f799ab
Danny Smooke <dana.smooke@gmail.com>
2021-01-28 13:20:07 -0500

    added const fs and util for inquirer, added employee_db under connection

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
 
 const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
     password: "password",
-    database: "greatBay_db"
+    database: "employee_db"
 });
 
 connection.connect((error) => {
