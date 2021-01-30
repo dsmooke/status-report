3b8339f92a762ab48a00dbf793d8300141f3591a
Danny Smooke <dana.smooke@gmail.com>
2021-01-30 12:09:54 -0500

    removed inquirer and express consts, exported a module

--- questions.js
+++ questions.js
@@ -1,66 +1,61 @@
-const inquirer = require("inquirer");
-const fs = require("fs");
-const util = require("util");
+module.exports = [
+  {
+    name: "doWhat",
+    type: "list",
+    message: "What would you like to do?",
+    choices: [
+      "View All Employees",
+      "View All Employees By Department",
+      "View All Employees By Manager",
+      "Add Employee",
+      "Remove Employee",
+      "View All Roles",
+      "Add Role",
+      "Remove Role",
+      "Update Employee Role",
+      "Update Employee Manager",
+    ],
+  },
 
-const questions = [
-    {
-        name: "doWhat",
-        type: "list",
-        message: "What would you like to do?",
-        choices: [
-            "View All Employees",
-            "View All Employees By Department",
-            "View All Employees By Manager",
-            "Add Employee",
-            "Remove Employee",
-            "View All Roles",
-            "Add Role",
-            "Remove Role",
-            "Update Employee Role",
-            "Update Employee Manager"
-        ]
+  {
+    name: "firstName",
+    type: "input",
+    message: "What is the employee's first name?",
+  },
+  {
+    name: "lastName",
+    type: "input",
+    message: "What is the employee's last name?",
+  },
+  {
+    name: "employeeRole",
+    type: "list",
+    message: "What is the employee's role?",
+    choices: [],
+  },
+  {
+    name: "removeEmployee",
+    type: "list",
+    message: "Which employees do you want to remove?",
+    choices: [],
+  },
+  {
+    name: "firstName",
+    type: "list",
+    message: "Who is the employee's manager?",
+    choices: [],
+  },
+  {
+    name: "updateManager",
+    type: "list",
+    message: "Which employee's manager do you want to update?",
+    choices: [],
+  },
 
-    },
-
-    {
-        name: "firstName",
-        type: "input",
-        message: "What is the employee's first name?",
-    },
-    {
-        name: "lastName",
-        type: "input",
-        message: "What is the employee's last name?",
-    },
-    {
-        name: "employeeRole",
-        type: "list",
-        message: "What is the employee's role?",
-        choices: []
-    },
-    {
-        name: "removeEmployee",
-        type: "list",
-        message: "Which employees do you want to remove?",
-        choices: []
-    },
-    {
-        name: "firstName",
-        type: "list",
-        message: "Who is the employee's manager?",
-        choices: []
-    },
-    {
-        name: "updateManager",
-        type: "list",
-        message: "Which employee's manager do you want to update?",
-        choices: []
-    },
-
-    {
-        name: "removeRole",
-        type: "list",
-        message: "What role would you like to remove?",
-        choices: []
-    },
+  {
+    name: "removeRole",
+    type: "list",
+    message: "What role would you like to remove?",
+    choices: [],
+  },
 ];
