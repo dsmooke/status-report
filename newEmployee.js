ccbea9d44fbcc17f0c55e83b00cd7d3040164ce8
Danny Smooke <dana.smooke@gmail.com>
2021-01-31 18:41:00 -0500

    created class of newEmployee.js that would be linked to app.js via path and be ina lib folder

---
+++ newEmployee.js
@@ -0,0 +1,15 @@
+class newEmployee {
+    constructor(id, first_name, last_name, title, manager) {
+        this.id = id;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.title = title;
+        this.manager = manager
+    }
+
+printInfo() {
+    return newEmployee
+    };
+};
+
+module.exports = newEmployee;
