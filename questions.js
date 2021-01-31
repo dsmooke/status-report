af641ac891acf5b7a6fa8f89133659fbbe40d372
Danny Smooke <dana.smooke@gmail.com>
2021-01-31 18:32:19 -0500

    commented out bonus questions

--- questions.js
+++ questions.js
@@ -7,15 +7,15 @@ module.exports = [
     message: "What would you like to do?",
     choices: [
       "View All Employees",
-      "View All Employees By Department",
-      "View All Employees By Manager",
+      "View All Departments",
+      // "View All Employees By Manager",
       "Add Employee",
-      "Remove Employee",
+      // "Remove Employee",
       "View All Roles",
       "Add Role",
-      "Remove Role",
+      // "Remove Role",
       "Update Employee Role",
-      "Update Employee Manager",
+      // "Update Employee Manager",
     ]
   },
 
