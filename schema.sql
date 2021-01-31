b10a5f8457cf8ef1af587747dcbe1e01039d7eb3
Danny Smooke <dana.smooke@gmail.com>
2021-01-31 18:30:26 -0500

    rearranged tables

--- schema.sql
+++ schema.sql
@@ -7,6 +7,16 @@ CREATE DATABASE IF NOT EXISTS employee_db;
 -- Use the DB "employee_db" for all the rest of the script
 USE employee_db;
 
+-- Created the table "employee"
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL, 
+    last_name VARCHAR(50) NOT NULL, 
+    role_id INTEGER default 0, 
+    manager_id INTEGER default 0, 
+    PRIMARY KEY(id)
+);
+
 -- Created the table "department"
 CREATE TABLE department (
 id INT NOT NULL AUTO_INCREMENT,
@@ -19,19 +29,10 @@ CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30), 
     salary DECIMAL, 
-    department_id INT
+    department_id INT,
     PRIMARY KEY (id)
 );
 
--- Created the table "employee"
-CREATE TABLE employee (
-    id INT NOT NULL AUTO_INCREMENT,
-    first_name VARCHAR(30) NOT NULL, 
-    last_name VARCHAR(50) NOT NULL, 
-    role_id INTEGER default 0, 
-    manager_id INTEGER default 0, 
-    PRIMARY KEY(id)
-);
 
 -- Inserted a set of records into the table
 INSERT INTO employee (id, first_name, last_name, role_id, salary, manager_id)
