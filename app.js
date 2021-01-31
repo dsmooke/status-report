c797d77778b11b94a9a853d9f3dfb0c1e7434fb5
Danny Smooke <dana.smooke@gmail.com>
2021-01-31 18:35:25 -0500

    deleted console.table for now

--- app.js
+++ app.js
@@ -28,38 +28,16 @@ const promptUser = async () => {
   // const newEmployee = (questions.firstName, questions.lastName, questions.employeeRole, questions.salary, questions.employeeDept, questions.employeeManager)
 };
 
-function addEmployee() {
-  if (questions.doWhat === "Add Employee") {
+// function addEmployee() {
+//   if (questions.doWhat === "Add Employee") {
 
-    const employee = new Employee(questions.firstName, questions.lastName, questions.employeeRole, questions.salary, questions.employeeDept, questions.employeeManager)
-    employeeList.push(employee);
+//     const employee = new Employee(questions.firstName, questions.lastName, questions.employeeRole, questions.salary, questions.employeeDept, questions.employeeManager)
+//     employeeList.push(employee);
 
-    } 
-};
-addEmployee();
+//     } 
+// };
+// addEmployee();
 
-
-// const cTable = require("console.table");
-
-// console.table("Employees", [employees]);
-
-
-// var employees = [
-//   [1, "Zeus", "Jupiter", "Lightning Director", "Heavens", 1000, "Cronus Titan"],
-
-//   [2, "Poseiden", "Neptune", "Sea Director", "Sea", 2000, "Cronus Titan"],
-  
-//   [3, "Hades", "Pluto", "Underworld Director", "Underworld", 3000, "Cronus Titan"],
-// ];
-
-// console.table(
-//   ["id", "first_name", "last_name", "title", "department", "salary", "manager"], employees);
-
-// console.log(table);
-
-// async function init() {
-//   console.log("Initializing");
-// }
 
 const init = async () => {
   try{
