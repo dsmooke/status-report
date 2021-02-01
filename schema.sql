0e8e89200eb0e7a8d0d8d2f20bdcc6b1ca23105e
Danny Smooke <dana.smooke@gmail.com>
2021-02-01 11:50:14 -0500

    adding insert into values with ids and manager ids

--- schema.sql
+++ schema.sql
@@ -8,7 +8,7 @@ CREATE DATABASE IF NOT EXISTS employee_db;
 USE employee_db;
 
 -- Created the table "employee"
-CREATE TABLE employee (
+CREATE TABLE employees (
     id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL, 
     last_name VARCHAR(50) NOT NULL, 
@@ -34,11 +34,32 @@ CREATE TABLE role (
 );
 
 
+-- Inserted a set of records into the table, columns in ()
+INSERT INTO employee (id, first_name, last_name, role_id, salary, manager_id)
+VALUES (1, "Zeus", "Jupiter", "Lightning Director", 1000, 1);
+
+INSERT INTO employee (id, first_name, last_name, role_id, salary, manager_id)
+VALUES (2, "Posiedon", "Neptune", "Sea Director", 2000, 1);
+
+INSERT INTO employee (id, first_name, last_name, role_id, salary, manager_id)
+VALUES (3, "Hades", "Pluto", "Underworld Director", 3000, 1);
+
+-- INSERT INTO department (id, name)
+-- VALUES (1, "Heavens");
+
+-- INSERT INTO role (id, title, salary, department_id)
+-- VALUES (1, "Lightning Director", 911000, "Heavens");
+
+
+
+
+
+
 -- show ALL employees
 SELECT id, first_name, last_name
-FROM employee
-INNER JOIN role ON employee.role_id = role.id;
+FROM employees
+INNER JOIN role ON employees.role_id = role.id;
 
 SELECT id, name
 FROM department
-RIGHT JOIN department ON employee.role_id = role.id;
+RIGHT JOIN department ON employees.role_id = role.id;
