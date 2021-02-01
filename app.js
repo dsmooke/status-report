817200d96ebdc34c73ebdb940ae82066b10aaed1
Danny Smooke <dana.smooke@gmail.com>
2021-02-01 12:24:52 -0500

    created viewEmployee function, added function and console.log in connection

--- app.js
+++ app.js
@@ -17,10 +17,26 @@ const connection = mysql.createConnection({
 connection.connect((error) => {
   if (error) throw error;
   console.log("Connected to Database");
+
+  // console.log("Viewing employee list \n");
+  // viewEmployees();
+
   // init();
 });
 
 // let employeeList = [];
+
+// view Employee function
+function viewEmployees() {
+  console.log("Viewing employee list...\n");
+  connection.query("SELECT * FROM employees", function (err, res) {
+    if (err) throw err;
+    // Log all results of the SELECT statement
+    console.log(res);
+    connection.end();
+  });
+  // viewEmployees();
+}
 
 const promptUser = async () => {
   const answers = await inquirer.prompt(questions);
