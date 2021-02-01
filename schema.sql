1d3a769bd2d0fde978ee7ea19e15d8c5ab4678ef
Danny Smooke <dana.smooke@gmail.com>
2021-02-01 13:08:45 -0500

    edited name of table for insert into, added slect * from employees, commented out show all employees join - displayed zeus, posiedon and hades but not aphrodite

--- schema.sql
+++ schema.sql
@@ -2,7 +2,7 @@
 DROP DATABASE IF EXISTS employee_db;
 
 -- Created the BD "employee_db"
-CREATE DATABASE IF NOT EXISTS employee_db;
+CREATE DATABASE employee_db;
 
 -- Use the DB "employee_db" for all the rest of the script
 USE employee_db;
@@ -19,9 +19,9 @@ CREATE TABLE employees (
 
 -- Created the table "department"
 CREATE TABLE department (
-id INT NOT NULL AUTO_INCREMENT,
-name VARCHAR(30),
-PRIMARY KEY (id)
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30),
+    PRIMARY KEY (id)
 );
 
 -- Created the table "role"
@@ -35,14 +35,14 @@ CREATE TABLE role (
 
 
 -- Inserted a set of records into the table, columns in ()
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES (1, "Zeus", "Jupiter", "Lightning Director", 1, 1);
+INSERT INTO employees (id, first_name, last_name, role_id, manager_id)
+VALUES (1, "Zeus", "Jupiter", 1, 1);
 
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES (2, "Posiedon", "Neptune", "Sea Director", 2, 1);
+INSERT INTO employees (id, first_name, last_name, role_id, manager_id)
+VALUES (2, "Posiedon", "Neptune", 2, 1);
 
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES (3, "Hades", "Pluto", "Underworld Director", 3, 1);
+INSERT INTO employees (id, first_name, last_name, role_id, manager_id)
+VALUES (3, "Hades", "Pluto", 3, 1);
 
 -- INSERT INTO department (id, name)
 -- VALUES (1, "Heavens");
@@ -51,15 +51,15 @@ VALUES (3, "Hades", "Pluto", "Underworld Director", 3, 1);
 -- VALUES (1, "Lightning Director", 911000, "Heavens");
 
 
+SELECT * FROM employees;
 
 
 
+-- -- show ALL employees
+-- SELECT id, first_name, last_name
+-- FROM employees
+-- INNER JOIN role ON employees.role_id = role.id;
 
--- show ALL employees
-SELECT id, first_name, last_name
-FROM employees
-INNER JOIN role ON employees.role_id = role.id;
-
-SELECT id, name
-FROM department
-RIGHT JOIN department ON employees.role_id = role.id;
+-- SELECT id, name
+-- FROM department
+-- RIGHT JOIN department ON employees.role_id = role.id;
