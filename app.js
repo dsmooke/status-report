cf304502ea37ac2ece12380569a6ddb2fd0c59f8
Danny Smooke <dana.smooke@gmail.com>
2021-02-02 14:03:30 -0500

    deleted connection and localhost info because now in EMS_db.js file within db folder

--- app.js
+++ app.js
@@ -1,64 +1,7 @@
-const mysql = require("mysql");
-const inquirer = require("inquirer");
-const fs = require("fs");
-const util = require("util");
+const connection = require("./db/EMS_db");
 
 const questions = require("./lib/questions");
 // console.log(questions.find((item) => item.name === "firstName")); // access questions
-
-// Lets the port be set by Heroku
-const PORT = process.env.PORT || 3306;
-
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: PORT,
-  user: "root",
-  password: "password",
-  database: "employee_db",
-});
-
-connection.connect((error) => {
-  if (error) throw error;
-  console.log(`Connected to Employee Database at Port ${PORT} \n`);
-
-  // console.log("Viewing employee list. \n");
-  // setTimeout(() => {
-  //   viewEmployees();
-  // }, 1000);
-  // viewEmployees();
-
-  // console.log("Adding new employee. \n");
-  // setTimeout(() => {
-  //   addEmployee();
-  // }, 1000);
-
-  // console.log("Viewing employee list. \n");
-  // setTimeout(() => {
-  //   viewEmployees();
-  // }, 1000);
-
-  // console.log("Updating employee's role. \n");
-  // setTimeout(() => {
-  //   updateRole();
-  // }, 1000);
-
-  // console.log("Viewing employee list. \n");
-  // setTimeout(() => {
-  //   viewEmployees();
-  // }, 1000);
-
-  // console.log("Deleting employee.\n");
-  // setTimeout(() => {
-  //   deleteEmployee();
-  // }, 1000);
-
-  // // console.log("Viewing employee list. \n");
-  // setTimeout(() => {
-  //   viewEmployees();
-  // }, 1000);
-
-  // init();
-});
 
 // let employeeList = [];
 
