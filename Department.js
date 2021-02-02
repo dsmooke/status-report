f2084a2d1cf0895056ffe7f45b1d116f1a395bed
Danny Smooke <dana.smooke@gmail.com>
2021-02-02 13:27:52 -0500

    created Department class with constructors, import Employee and Role classes

---
+++ Department.js
@@ -0,0 +1,10 @@
+const Employee = require("");
+const Role = require("");
+
+class Department {
+  constructor(id, name) {
+    (this.id = id), (this.name = name);
+  }
+}
+
+module.exports = Department;
